cb43c5bf865b4b69e34f6112bb8746367ae61402
Rashmi Yadav <rays.rashmi@gmail.com>
2017-03-21 19:09:17 +0530

    First commit

---
+++ src/chat_app.cr
@@ -0,0 +1,28 @@
+require "./chat_app/*"
+require "kemal"
+
+SOCKETS = [] of HTTP::WebSocket
+ws "/chat" do |socket|
+  # Add the client to SOCKETS list
+  SOCKETS << socket
+
+  # Broadcast each message to all clients
+  socket.on_message do |message|
+    SOCKETS.each { |socket| socket.send message }
+  end
+
+  # Remove clients from the list when it's closed
+  socket.on_close do
+    SOCKETS.delete socket
+  end
+end
+
+get "/" do
+  render "views/index.ecr"
+end
+
+module ChatApp
+  # TODO Put your code here
+end
+
+Kemal.run
